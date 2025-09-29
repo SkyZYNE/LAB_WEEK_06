d3d4989ae39a1197f4851a9915809bcd6a0e55f5
SkyZYNE <raffaelgunawan19@gmail.com>
2025-09-29 15:06:53 +0700

    Commit No.2 - Clickable RecyclerView

--- app/src/main/java/com/example/lab_week_06/CatAdapter.kt
+++ app/src/main/java/com/example/lab_week_06/CatAdapter.kt
@@ -7,25 +7,34 @@ import com.example.lab_week_06.model.CatModel
 
 class CatAdapter(
     private val layoutInflater: LayoutInflater,
-    private val imageLoader: ImageLoader
+    private val imageLoader: ImageLoader,
+    private val onClickListener: OnClickListener
 ) : RecyclerView.Adapter<CatViewHolder>() {
-
+    //Mutable list for storing all the list data
     private val cats = mutableListOf<CatModel>()
-
+    //A function to set the mutable list
     fun setData(newCats: List<CatModel>) {
         cats.clear()
         cats.addAll(newCats)
+        //This is used to tell the adapter that there's a data change in the mutable list
         notifyDataSetChanged()
     }
-
+    //A view holder is used to bind the data to the layout views
+    //onCreateViewHolder is instantiating the view holder it self
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CatViewHolder {
         val view = layoutInflater.inflate(R.layout.item_list, parent, false)
-        return CatViewHolder(view, imageLoader)
+        return CatViewHolder(view, imageLoader, onClickListener)
     }
-
+    //This is used to get the amount of data/item in the list
     override fun getItemCount() = cats.size
-
+    //This is used to bind each data to each layout views
     override fun onBindViewHolder(holder: CatViewHolder, position: Int) {
+        //The holder parameter stores our previously created ViewHolder
+        //The holder.bindData function is declared in the CatViewHolder
         holder.bindData(cats[position])
+    }
+    //Declare an onClickListener interface
+    interface OnClickListener {
+        fun onItemClick(cat: CatModel)
     }
 }
--- app/src/main/java/com/example/lab_week_06/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_06/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.lab_week_06.model.CatBreed
 import com.example.lab_week_06.model.CatModel
 import com.example.lab_week_06.model.Gender
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     private val recyclerView: RecyclerView by lazy {
@@ -14,18 +15,27 @@ class MainActivity : AppCompatActivity() {
     }
 
     private val catAdapter by lazy {
-        CatAdapter(layoutInflater, GlideImageLoader(this))
+        //Glide is used here to load the images
+        //Here we are passing the onClickListener function to the Adapter
+        CatAdapter(layoutInflater, GlideImageLoader(this), object:
+            CatAdapter.OnClickListener {
+            //When this is triggered, the pop up dialog will be shown
+            override fun onItemClick(cat: CatModel) = showSelectionDialog(cat)
+        })
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        //Setup the adapter for the recycler view
         recyclerView.adapter = catAdapter
+        //Setup the layout manager for the recycler view
+        //A layout manager is used to set the structure of the item views
+        //For this tutorial, we're using the vertical linear structure
         recyclerView.layoutManager = LinearLayoutManager(
             this, LinearLayoutManager.VERTICAL, false
         )
-
+        //Add data to the model list in the adapter
         catAdapter.setData(
             listOf(
                 CatModel(Gender.Male, CatBreed.BalineseJavanese, "Fred", "Silent and deadly", "https://cdn2.thecatapi.com/images/7dj.jpg"),
@@ -33,5 +43,15 @@ class MainActivity : AppCompatActivity() {
                 CatModel(Gender.Unknown, CatBreed.AmericanCurl, "Curious George", "Award winning investigator", "https://cdn2.thecatapi.com/images/bar.jpg")
             )
         )
+    }
+    //This will create a pop up dialog when one of the items from the recycler view is clicked.
+    private fun showSelectionDialog(cat: CatModel) {
+        AlertDialog.Builder(this)
+            //Set the title for the dialog
+            .setTitle("Cat Selected")
+            //Set the message for the dialog
+            .setMessage("You have selected cat ${cat.name}")
+            //Set if the OK button should be enabled
+            .setPositiveButton("OK") { _, _ -> }.show()
     }
 }
